5682622b0e0340d664a5e454d7b3fb9ec6d816aa
barona13 <baronaa13@hotmail.com>
2024-05-10 15:26:21 -0500

    first commit

---
+++ Creacionales/Singleton/src/main/java/com/singleton/Personas.java
@@ -0,0 +1,27 @@
+package com.singleton;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Personas {
+    private static Personas instance = null;
+    private List<String> listaPersonas;
+
+    private Personas() {
+        listaPersonas = Arrays.asList("Juan", "María", "Carlos");
+    }
+
+    public static Personas getInstance() {
+        if (instance == null) {
+            instance = new Personas();
+        }
+        return instance;
+    }
+
+    public void mostrarPersonas() {
+        System.out.println("lista de Personas:");
+        for (String persona : listaPersonas) {
+            System.out.println(persona);
+        }
+    }
+}
